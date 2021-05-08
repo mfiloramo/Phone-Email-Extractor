c3e039988dff7d607cd1afd85965ad4be7baaba6
mfiloramo <mlfiloramo@gmail.com>
2021-05-08 13:36:13 -0400

    Commit adjustment.

--- main.py
+++ main.py
@@ -14,9 +14,8 @@ emailRegex = re.compile(r'''(
             [a-zA-Z0-9._%+-]+                   # username
             @                                   # @ symbol
             [a-zA-Z0-9.-]+                      # domain name
-            (\.[a-zA-Z]{2,4})                  # dot-something
+            (\.[a-zA-Z]{2,4})                   # dot-something
             )''', re.VERBOSE)
-
 
 clipboard = str(pyperclip.paste())
 matches = []
